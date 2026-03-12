5da9ace3dc81088ad8fd3a8ba54cba3096e9367a
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-03-12 10:51:57 -0300

    feat: add calculate_age()

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -24,46 +24,46 @@ def filter_df(df: pd.DataFrame) -> pd.DataFrame:
     df.drop(['municipio'], axis=1, inplace=True)
 
     # Filtramos solo los inscriptos en etapa 1
-    form = df[df['etapa_inscripcion'] == 1 ]
+    df = df[df['etapa_inscripcion'] == 1 ]
 
     #Filtramos solo los inscriptos en estado solicitud_adjudicada ó solicitud_elegible_rechazadas_por_excedente 
-    form[form['state'] == 'solicitud_adjudicada']
-    form[form['state'] =='solicitud_elegible_rechazadas_por_excedente']
+    df[df['state'] == 'solicitud_adjudicada']
+    df[df['state'] =='solicitud_elegible_rechazadas_por_excedente']
 
     # Asignamos variable al filtro de los inscriptos en estado solicitud_adjudicada ó solicitud_elegible_rechazadas_por_excedente
-    filtro = (form['state'] == 'solicitud_adjudicada') | \
-             (form['state'] == 'solicitud_elegible_rechazadas_por_excedente')
+    filtro = (df['state'] == 'solicitud_adjudicada') | \
+             (df['state'] == 'solicitud_elegible_rechazadas_por_excedente')
 
     # Creamos el nuevo dataframe aplicando el filtro
-    form = form[filtro]
-    return form
+    df = df[filtro]
+    return df
 
 ## --------------------------- Cálculo de atributo faltante (edad) -----------------------------
-#
-# Como los valores de cada columna son de distinto tipo de dato, transformamos 'fecha_de_nacimiento' y 'fecha de_carga'
-form.loc['fecha_de_nacimiento'] = pd.to_datetime(
-    form['fecha_de_nacimiento'],
-    errors='coerce'
-)
+def calculate_age(df: pd.DataFrame) -> pd.DataFrame:
+    # Como los valores de cada columna son de distinto tipo de dato, transformamos 'fecha_de_nacimiento' y 'fecha de_carga'
+    df.loc['fecha_de_nacimiento'] = pd.to_datetime(
+        df['fecha_de_nacimiento'],
+        errors='coerce'
+    )
 
-form.loc['fecha_carga'] = pd.to_datetime(
-    form['fecha_carga'],
-    errors='coerce'
-)
+    df.loc['fecha_carga'] = pd.to_datetime(
+        df['fecha_carga'],
+        errors='coerce'
+    )
 
-#TODO:ver función lambda
-# Calcular edad aplicando relativedelta fila a fila
-form['edad'] = form.apply(
-    lambda row: relativedelta(row['fecha_carga'], row['fecha_de_nacimiento']).years 
-                if pd.notnull(row['fecha_de_nacimiento']) else pd.NA,
-    axis=1
-)
-
+    #TODO:ver función lambda
+    # Calcular edad aplicando relativedelta fila a fila
+    df['edad'] = df.apply(
+        lambda row: relativedelta(row['fecha_carga'], row['fecha_de_nacimiento']).years 
+                    if pd.notnull(row['fecha_de_nacimiento']) else pd.NA,
+        axis=1
+    )
+    return df
 # --------------------------......... Cálculo directo de media -----------------------------------------
 
-vuln = float(round(form['escenario_vulnerabilidad_social'].mean(skipna=True), 1))
+vuln = float(round(df['escenario_vulnerabilidad_social'].mean(skipna=True), 1))
 #paredes_ext_rev = float(round(formularios_estudio['paredes_ext_revocadas'].mean(skipna=True), 1))
-paredes_ext_rev = round(form['paredes_ext_revocadas'].mean(skipna=True), 1)
+paredes_ext_rev = round(df['paredes_ext_revocadas'].mean(skipna=True), 1)
 
 
 
