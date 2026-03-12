ac443d5347337ecdac82befef4d0d6b5ee53f645
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-03-12 10:35:49 -0300

    feat: add concatenate_df()

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,20 +6,22 @@ from dateutil.relativedelta import relativedelta
 
 
 # ---------------------------------------- pre-procesamiento ------------------------------
+def concatenate_df(df_1:pd.DataFrame, df_2: pd.DataFrame, df_3: pd.DataFrame) -> pd.DataFrame: 
+    # Descargamos el dataframe con información de los municipios, los inscriptos al programa y los formularios completos por dichos inscriptos
+    df_1= pd.read_excel("base_municipios.xlsx")
+    df_2 = pd.read_excel("ficha_inscriptos.xlsx")
+    df_3= pd.read_excel("formularios_curso.xlsx")
 
-# Descargamos el dataframe con información de los municipios, los inscriptos al programa y los formularios completos por dichos inscriptos
-df_muni = pd.read_excel("base_municipios.xlsx")
-df_insc = pd.read_excel("ficha_inscriptos.xlsx")
-df_form = pd.read_excel("formularios_curso.xlsx")
+    # Concatenamos dataframes por columna
+    df = pd.concat([df_1, df_2, df_3], axis=1)
 
-
-# Concatenamos dataframes por columna
-df_evaluate = pd.concat([df_muni, df_insc, df_form], axis=1)
-
-# Eliminamos columnas prescindibles, como 'municipio'
-df_evaluate.drop(['municipio'], axis=1, inplace=True)
+    # Eliminamos columnas prescindibles
+    df.drop(['municipio'], axis=1, inplace=True)
+    return df
 
 # --------------------------------------- Filtración ------------------------------------------
+
+
 
 # Filtramos solo los inscriptos en etapa 1
 formularios_estudio = df_evaluate[df_evaluate['etapa_inscripcion'] == 1 ]
@@ -63,32 +65,6 @@ vuln = float(round(formularios_estudio['escenario_vulnerabilidad_social'].mean(s
 #paredes_ext_rev = float(round(formularios_estudio['paredes_ext_revocadas'].mean(skipna=True), 1))
 paredes_ext_rev = round(formularios_estudio['paredes_ext_revocadas'].mean(skipna=True), 1)
 
-# ------------------------------------ Filtro inicial de candidatos por grupo ----------------------------
 
-# Se agrupa en 'Tratamiento' si se cumple 'solicitud_adjudicada', y 'control', en caso contrario
-formularios_estudio['grupo'] = np.where( formularios_estudio['state'] == 'solicitud_adjudicada','Tratamiento', 'Control')
-
-# Guardar extracción de datos en "Control" o "Tratamiento"
-grupo_control = (formularios_estudio['grupo']) == 'Control' # es una serie que devuelve booleanos
-grupo_tratamiento = (formularios_estudio['grupo']) == 'Tratamiento' # idem
-
-# A cada grupo le damos estructura de dataframe nuevo
-df_control = formularios_estudio[grupo_control]
-df_tratamiento = formularios_estudio[grupo_tratamiento]
-
-# Se eliminan columnas innecesarias en ambos dataframes
-df_control_clean = df_control.drop(columns=[
-    'codigo_municipio', 'codigo_region', 'nombre_region', 'codigo_area',
-    'nombre_area', 'seccion_electoral',
-    'superficie(km2)', 'intendente', 'partido_politico_actual', 
-    'poblacion_censo_2010', 'poblacion_censo_2022'
-])
-
-df_tratamiento_clean = df_tratamiento.drop(columns=[
-    'codigo_municipio', 'codigo_region', 'nombre_region', 'codigo_area',
-    'nombre_area', 'seccion_electoral',
-    'superficie(km2)', 'intendente', 'partido_politico_actual', 
-    'poblacion_censo_2010', 'poblacion_censo_2022'
-])
 
 # ---
