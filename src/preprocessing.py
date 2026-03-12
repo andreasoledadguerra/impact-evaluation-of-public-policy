171e1bc06fd69c0895e53594d634ac7351ce77bb
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-03-12 10:41:37 -0300

    refactor: delete df.drop in concatenate_df()

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -14,46 +14,46 @@ def concatenate_df(df_1:pd.DataFrame, df_2: pd.DataFrame, df_3: pd.DataFrame) ->
 
     # Concatenamos dataframes por columna
     df = pd.concat([df_1, df_2, df_3], axis=1)
-
-    # Eliminamos columnas prescindibles
-    df.drop(['municipio'], axis=1, inplace=True)
+    
     return df
 
 # --------------------------------------- Filtración ------------------------------------------
 
-
-
-# Filtramos solo los inscriptos en etapa 1
-formularios_estudio = df_evaluate[df_evaluate['etapa_inscripcion'] == 1 ]
-
-#Filtramos solo los inscriptos en estado solicitud_adjudicada ó solicitud_elegible_rechazadas_por_excedente 
-formularios_estudio[formularios_estudio['state'] == 'solicitud_adjudicada']
-formularios_estudio[formularios_estudio['state'] =='solicitud_elegible_rechazadas_por_excedente']
-
-# Asignamos variable al filtro de los inscriptos en estado solicitud_adjudicada ó solicitud_elegible_rechazadas_por_excedente
-filtro = (formularios_estudio['state'] == 'solicitud_adjudicada') | \
-         (formularios_estudio['state'] == 'solicitud_elegible_rechazadas_por_excedente')
-
-# Creamos el nuevo dataframe aplicando el filtro
-formularios_estudio = formularios_estudio[filtro]
-
-
-# --------------------------- Cálculo de atributo faltante (edad) -----------------------------
-
+#def filter_df(df: pd.DataFrame) -> pd.DataFrame:
+#
+#    df.drop(['municipio'], axis=1, inplace=True)
+#
+#    # Filtramos solo los inscriptos en etapa 1
+#    form = df[df['etapa_inscripcion'] == 1 ]
+#
+#    #Filtramos solo los inscriptos en estado solicitud_adjudicada ó solicitud_elegible_rechazadas_por_excedente 
+#    form[form['state'] == 'solicitud_adjudicada']
+#    form[form['state'] =='solicitud_elegible_rechazadas_por_excedente']
+#
+#    # Asignamos variable al filtro de los inscriptos en estado solicitud_adjudicada ó solicitud_elegible_rechazadas_por_excedente
+#    filtro = (form['state'] == 'solicitud_adjudicada') | \
+#             (form['state'] == 'solicitud_elegible_rechazadas_por_excedente')
+#
+#    # Creamos el nuevo dataframe aplicando el filtro
+#    form = form[filtro]
+#    return form
+#
+## --------------------------- Cálculo de atributo faltante (edad) -----------------------------
+#
 # Como los valores de cada columna son de distinto tipo de dato, transformamos 'fecha_de_nacimiento' y 'fecha de_carga'
-formularios_estudio.loc['fecha_de_nacimiento'] = pd.to_datetime(
-    formularios_estudio['fecha_de_nacimiento'],
+form.loc['fecha_de_nacimiento'] = pd.to_datetime(
+    form['fecha_de_nacimiento'],
     errors='coerce'
 )
 
-formularios_estudio.loc['fecha_carga'] = pd.to_datetime(
-    formularios_estudio['fecha_carga'],
+form.loc['fecha_carga'] = pd.to_datetime(
+    form['fecha_carga'],
     errors='coerce'
 )
 
 #TODO:ver función lambda
 # Calcular edad aplicando relativedelta fila a fila
-formularios_estudio['edad'] = formularios_estudio.apply(
+form['edad'] = form.apply(
     lambda row: relativedelta(row['fecha_carga'], row['fecha_de_nacimiento']).years 
                 if pd.notnull(row['fecha_de_nacimiento']) else pd.NA,
     axis=1
@@ -61,9 +61,9 @@ formularios_estudio['edad'] = formularios_estudio.apply(
 
 # --------------------------......... Cálculo directo de media -----------------------------------------
 
-vuln = float(round(formularios_estudio['escenario_vulnerabilidad_social'].mean(skipna=True), 1))
+vuln = float(round(form['escenario_vulnerabilidad_social'].mean(skipna=True), 1))
 #paredes_ext_rev = float(round(formularios_estudio['paredes_ext_revocadas'].mean(skipna=True), 1))
-paredes_ext_rev = round(formularios_estudio['paredes_ext_revocadas'].mean(skipna=True), 1)
+paredes_ext_rev = round(form['paredes_ext_revocadas'].mean(skipna=True), 1)
 
 
 
