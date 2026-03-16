902ec84b237501569c75f5c5e2e45613a941bacb
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-03-16 12:00:31 -0300

    feat: add method to calculate mean of a bootstrap sample

--- src/bootstrapping.py
+++ src/bootstrapping.py
@@ -6,3 +6,7 @@ import numpy as np
 def get_sample_bootstrap(df_sample: pd.DataFrame, column_df_sample: str, random_state: int = 42) -> np.ndarray:
     np.random.seed(random_state)
     return np.random.choice(df_sample[column_df_sample], size=len(df_sample), replace=True)
+
+# Función que calcula la media de una lista de datos obtenido por bootstrapping
+def calculate_mean_bootstrap(bootstrap_samples: np.ndarray) -> float:
+    return np.mean(bootstrap_samples, axis=0)
