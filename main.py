19c3c8530ba8da0cee0a2c72d9f97dfbedb112cb
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-03-16 10:58:51 -0300

    feat:  add method to apply statistical calculations to each sample

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from src.utils import Stats
 sample_analysis = SampleAnalysis()
 
 SAMPLE_SIZE = 1000
+
 # Generar muestras aleatorias del grupo control y tratamiento 
 def generate_samples(df_control:pd.DataFrame, df_treatment: pd.DataFrame)-> tuple[pd.DataFrame, pd.DataFrame]:
     srs_c= simple_random_sample(df_control, SAMPLE_SIZE)
@@ -17,30 +18,13 @@ def generate_samples(df_control:pd.DataFrame, df_treatment: pd.DataFrame)-> tupl
 
 #recibe los dataframes de generate_sample
 # y hace cálculos estadísticos sobre ciertas variables(columnas)
-#def compute_sample_statistics(tuple[pd.DataFrame, pd.DataFrame]) -> tuple[pd.DataFrame,pd.DataFrame]:
-#
-#    calculate_media_condition(df,)
-#    calculate_std_condition(df,)
-#
-#    calculate_media_round(df, 'escenario_vulnerabilidad_social')
-#    calculate_media_round (df, 'paredes_ext_revocadas')
-#
-#    return df
-#
-#
-#
-#
-#calculate_media_round(df, 'escenario_vulnerabilidad_social')
-#calculate_media_round (df, 'paredes_ext_revocadas')
-#
-#df_media_condición_control = calcular_media_condicion(df_control_clean, dict_condiciones)
-#df_media_std_control = calcular_media_std_lista(df_control_clean, lista_c)
-#
-#fila_vuln_c = pd.DataFrame({'escenario_vulnerabilidad_social': [vuln_control]}, index= [1])
-#fila_per_c = pd.DataFrame({'paredes_ext_revocadas': [paredes_ext_rev_control]}, index= [1])
-#
-#
-#analysis = SampleAnalysis(df=treatment_group)
+def compute_sample_statistics(data: tuple[pd.DataFrame, pd.DataFrame]) -> tuple[pd.DataFrame,pd.DataFrame]:
+    df_control, df_treatment = data #desempaquetado para poder aplicar los métodos
+    stats_c = SampleAnalysis(df_control)
+    stats_t = SampleAnalysis(df_treatment)
+
+    return stats_c, stats_t
+
 #
 ## Usando las condiciones default de la clase
 #analysis.sample_media_condition()
