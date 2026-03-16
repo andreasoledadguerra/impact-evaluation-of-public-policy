9d2dbf7e37edf03102519cf88c9a6bce0a694ca2
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-03-16 11:44:43 -0300

    feat: add method to get sample for bootstrapping

--- src/bootstrapping.py
+++ src/bootstrapping.py
@@ -3,6 +3,6 @@ import numpy as np
 
 
  # Función para obtener una muestra bootstrap
-    #def get_sample_bootstrap(df_sample: pd.DataFrame, column_df_sample: str, random_state: int = 42) -> np.ndarray:
-    #    np.random.seed(random_state)
-    #    return np.random.choice(df_sample[column_df_sample], size=len(df_sample), replace=True)
+def get_sample_bootstrap(df_sample: pd.DataFrame, column_df_sample: str, random_state: int = 42) -> np.ndarray:
+    np.random.seed(random_state)
+    return np.random.choice(df_sample[column_df_sample], size=len(df_sample), replace=True)
