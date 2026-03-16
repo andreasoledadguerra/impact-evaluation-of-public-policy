8fb4f68c96d0eccc7353ddbdc2589cc1483937f0
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-03-16 11:43:15 -0300

    chore:  create file and import pandas

---
+++ src/bootstrapping.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+ # Función para obtener una muestra bootstrap
+    #def get_sample_bootstrap(df_sample: pd.DataFrame, column_df_sample: str, random_state: int = 42) -> np.ndarray:
+    #    np.random.seed(random_state)
+    #    return np.random.choice(df_sample[column_df_sample], size=len(df_sample), replace=True)
