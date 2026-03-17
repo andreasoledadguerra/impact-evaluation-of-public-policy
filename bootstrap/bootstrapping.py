30924c08f54ad106c2b3c86bb603fd9ad21f5256
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-03-17 12:42:55 -0300

    chore: create folder bootstrap, and move from src bootstrapping.py

--- bootstrap/bootstrapping.py
+++ bootstrap/bootstrapping.py
@@ -8,5 +8,8 @@ def get_sample_bootstrap(df_sample: pd.DataFrame, column_df_sample: str, random_
     return np.random.choice(df_sample[column_df_sample], size=len(df_sample), replace=True)
 
 # Función que calcula la media de una lista de datos obtenido por bootstrapping
-def calculate_mean_bootstrap(bootstrap_samples: np.ndarray) -> float:
-    return np.mean(bootstrap_samples, axis=0)
+def calculate_bootstrap_stats(bootstrap_samples: np.ndarray) -> dict:
+    return {
+        
+    }
+        np.mean(bootstrap_samples)
