a874b93bfe9cd72e565192186a89792d82bcf911
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-03-18 11:42:23 -0300

    chore: add comment to var's Field()

--- bootstrap/models.py
+++ bootstrap/models.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 
 class BootsrapStats(BaseModel):
     mean: float = Field (..., description="Media de la muestra bootstrap")
-    var: float = Field ( ..., ge=0, description="Varianza (ddof=1)")
+    var: float = Field ( ..., ge=0, description="Varianza (ddof=1)") # sample variance
 
     class Config:
         frozen = True
