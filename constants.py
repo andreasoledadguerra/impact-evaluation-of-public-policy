5cda2b9ee4e218419b9dfb23ad61a680793d907e
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-14 11:22:01 -0300

    docs: add file with variables as constants

---
+++ constants.py
@@ -0,0 +1,8 @@
+NUM_COLUMNS = ['ingreso_anual_hogar', 'edad', 'personas_por_ambiente']
+
+CAT_CONDITIONS = {
+    'sexo_dni': ['F', 'M'],
+    'relación_de_parentezco_con jefe_del_hogar': ['Soy jefa(e)'],
+    'conurbano_interior': ['Conurbano']
+}
+SPC_COLUMNS = ['escenario_vulnerabilidad_social','paredes_ext_revocadas' ]
