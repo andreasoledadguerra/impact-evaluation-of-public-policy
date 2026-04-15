60736ca83170311db92b39346275198671f8045b
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-15 09:27:18 -0300

    refactor: move simple_random_sample() to randomization.py

--- main.py
+++ main.py
@@ -15,24 +15,25 @@ sample_analysis = SampleAnalysis()
 
 SAMPLE_SIZE = 1000
 
-# Generar muestras aleatorias del grupo control y tratamiento 
-def generate_samples(df_control:pd.DataFrame, df_treatment: pd.DataFrame)-> tuple[pd.DataFrame, pd.DataFrame]:
-    srs_c= simple_random_sample(df_control, SAMPLE_SIZE)
-    srs_t = simple_random_sample(df_treatment,SAMPLE_SIZE)
-    return srs_c, srs_t  #----> dos muestras en forma de df
 
+
+
+# Generar muestras aleatorias del grupo control y tratamiento (paso previo al bootstrapping)
+
+#
 
 #recibe los dataframes de generate_sample
 # y hace cálculos estadísticos sobre ciertas variables(columnas)
-def compute_sample_statistics(data: tuple[pd.DataFrame, pd.DataFrame]) -> tuple[pd.DataFrame,pd.DataFrame]:
+def compute_sample_statistics_first(data: tuple[pd.DataFrame, pd.DataFrame]) -> tuple[pd.DataFrame,pd.DataFrame]:
     df_control, df_treatment = data #desempaquetado para poder aplicar los métodos
     stats_c = SampleAnalysis(df_control)
     stats_t = SampleAnalysis(df_treatment)
 
     return stats_c, stats_t
 
-# -------------------------------- BOOTSTRAPPING ('ingreso_anual_hogar', etc)-----------------------------------------
+# -------------------------------- BOOTSTRAPPING OVER NUM_COLUMNS, CAT_COLUMNS & SPC_COLUMNS-----------------------------------------
 
+columns = [NUM_COLUMNS, CAT_CONDITIONS , SPC_COLUMNS]
 
 experiment = BootstrapExperiment(
     data = (df_control, df_treatment),
--- src/randomization.py
+++ src/randomization.py
@@ -25,12 +25,17 @@ def randomization(df: pd.DataFrame) -> pd.DataFrame:
 
     return df_control, df_treatment
 
-
 #------------------------------------- Muestreo ------------------------------------
-# Crear una función para calcular muestra
+ # Crear una función para calcular muestra
 def simple_random_sample(df: pd.DataFrame, n:int, seed=42 ):
-    df = df.sample(n=n,random_state=seed)
-    return df 
+     df = df.sample(n=n,random_state=seed)
+     return df 
 
+#Método para extraer muestra antes del bootstrapping
+def generate_samples_first(df_control:pd.DataFrame, df_treatment: pd.DataFrame)-> tuple[pd.DataFrame, pd.DataFrame]:
+    srs_c= simple_random_sample(df_control, SAMPLE_SIZE)
+    srs_t = simple_random_sample(df_treatment,SAMPLE_SIZE)
+    return srs_c, srs_t  #----> dos muestras en forma de df
+    
 
 
