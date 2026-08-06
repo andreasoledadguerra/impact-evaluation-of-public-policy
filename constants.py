85056868b2a2585490fd6393a883cb92b7854e74
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-08-05 21:31:35 -0300

    fix: correct typo

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ NUM_COLUMNS = ['ingreso_anual_hogar', 'edad', 'personas_por_ambiente']
 
 CAT_CONDITIONS = {
     'sexo_dni': ['F', 'M'],
-    'relación_de_parentezco_con jefe_del_hogar': ['Soy jefa(e)'],
+    'relación_de_parentezco_con_jefe_del_hogar': ['Soy jefa(e)'],
     'conurbano_interior': ['Conurbano']
 }
 SPC_COLUMNS = ['escenario_vulnerabilidad_social','paredes_ext_revocadas' ]
