74877b8f97690369b6923c107dbbff855ec6c718
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-08-18 11:58:55 -0300

    docs: add comments to the variables to indicate which database they come from

--- constants.py
+++ constants.py
@@ -1,10 +1,16 @@
 SAMPLE_SIZE = 1000 
 
-NUM_COLUMNS = ['ingreso_anual_hogar', 'edad', 'personas_por_ambiente']
+NUM_COLUMNS = ['ingreso_anual_hogar', # FORMULARIOS_PATH
+                'edad', # FORMULARIOS_PATH, calculated in preprocessing.py using fecha_de_nacimiento and fecha_carga
+                'personas_por_ambiente' # FORMULARIOS_PATH
+]
 
 CAT_CONDITIONS = {
-    'sexo_dni': ['F', 'M'],
-    'relacion_de_parentezco_con_jefe_del_hogar': ['Soy jefa(e)'],
-    'conurbano_interior': ['Conurbano']
+    'sexo_dni': ['F', 'M'], # FORMULARIOS_PATH
+    'relacion_de_parentezco_con_jefe_del_hogar': ['Soy jefa(e)'], # FORMULARIOS_PATH
+    'conurbano_interior': ['Conurbano'] # MUNICIPIOS_PATH
 }
-SPC_COLUMNS = ['escenario_vulnerabilidad_social','paredes_ext_revocadas' ]
+
+SPC_COLUMNS = ['escenario_vulnerabilidad_social', # FORMULARIOS_PATH
+               'paredes_ext_revocadas' # FORMULARIOS_PATH
+]
