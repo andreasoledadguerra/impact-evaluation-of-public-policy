05f7c7e907eeb2914627f9c93e20c697df62b708
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-08-25 12:16:57 -0300

    docs: import constants and utils to the file

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 import numpy as np
 from pydantic import BaseModel, Field, model_validator
+from src.utils import _calculate_media_std, calculate_media_condition
+from constants import NUM_COLUMNS, CAT_CONDITIONS, SPC_COLUMNS
 
 class Stats(BaseModel, frozen=True):
   column: str
