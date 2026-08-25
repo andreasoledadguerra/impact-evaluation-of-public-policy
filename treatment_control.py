9890861de29c82c9bc515381af46607b6da4076e
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-08-25 11:15:18 -0300

    docs:delete pandas and import pydantic and elements from constants.py and utils.py

--- treatment_control.py
+++ treatment_control.py
@@ -1,1 +1,5 @@
-import pandas as pd
+from pydantic import BaseModel, Field, model_validator
+from src.utils import _calculate_media_std
+from constants import NUM_COLUMNS, CAT_CONDITIONS, SPC_COLUMNS
+# recibo     df_control, df_treatment = randomization(processed_df.df)
+
