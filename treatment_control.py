a53a8861d13397d1bb1520618c849b0b47bdd984
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-08-25 11:53:26 -0300

    feat: add class TreatmentControlSummary

--- treatment_control.py
+++ treatment_control.py
@@ -1,5 +1,14 @@
+from models import Stats, Proportions
 from pydantic import BaseModel, Field, model_validator
 from src.utils import _calculate_media_std
 from constants import NUM_COLUMNS, CAT_CONDITIONS, SPC_COLUMNS
-# recibo     df_control, df_treatment = randomization(processed_df.df)
+# recibo     df_control, df_treatment = randomization(processed_df.df
 
+
+class TreatmentControlSummary:
+
+    def __init__(self, tuple):
+        self.tuple = tuple
+        self.summary = self._calculate()
+
+    def _calculate(self) -> tuple[None, TreatmentControlType]:
