aa389377237aacfe906aaa2bdec9c39f864f9cb6
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-08-25 12:15:38 -0300

    feat: add class GroupSummary

--- models.py
+++ models.py
@@ -18,3 +18,44 @@ class Proportions(BaseModel, frozen=True):
     if not np.isclose(total_proportion, 1.0, atol=1e-5):
       raise ValueError(f"Total proportion must be 1.0, got {total_proportion}")
     return self
+
+
+PopulationStatType = Stats | Proportions
+
+class GruopSummary:
+
+  def __init__(self, df):
+    self.df = df
+    self.summary = self._calculate()
+
+  def _calculate(self) -> dict[str, PopulationStatType]:
+    result = {}
+
+    for col in NUM_COLUMNS + SPC_COLUMNS:
+      serie = self.df[col].dropna()
+      mean, std = _calculate_media_std(self.df, col)
+      result[col] = Stats(
+        column = col,
+        mean = mean,
+        std = std,
+        n = int(serie.count())
+      )
+
+    for col, categories in CAT_CONDITIONS.items():
+      serie = self.df[col].dropna()
+      filtered = serie[serie.isin(categories)]
+      props = (
+        filtered
+        .value_counts(normalize=True)
+        .round(4)
+        .to_dict()
+      )
+      result[col] = Proportions(
+        column = col,
+        proportions = {str(k): float(v) for k, v in props.items()},
+              n = int(serie.count())
+      )
+
+    return result
+
+  
