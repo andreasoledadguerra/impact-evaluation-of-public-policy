8d4dcef1ac8d742614b616fd572a4f461009db0e
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-08-25 11:55:50 -0300

    feat: join class Stats and Proportions into TreatmentControlType

--- treatment_control.py
+++ treatment_control.py
@@ -4,6 +4,7 @@ from src.utils import _calculate_media_std
 from constants import NUM_COLUMNS, CAT_CONDITIONS, SPC_COLUMNS
 # recibo     df_control, df_treatment = randomization(processed_df.df
 
+TreatmentControlType = Stats | Proportions
 
 class TreatmentControlSummary:
 
