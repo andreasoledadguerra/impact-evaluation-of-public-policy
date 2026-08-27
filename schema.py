eed92003d2737a9f64d45a8f8a0cdc487220dd9e
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-08-27 08:53:48 -0300

    docs: fix tabs

--- schema.py
+++ schema.py
@@ -71,8 +71,10 @@ class GroupSummary:
 
 
     return result
+
   
 class SubGroupSummary:
+
 
   def __init__(self,data:tuple[pd.DataFrame, pd.DataFrame]):
     df_control, df_treatment = data   #unpack the tuple
@@ -80,7 +82,6 @@ class SubGroupSummary:
     self.summary_treatment = GroupSummary(df_treatment).summary
 
   def get(self, group: str) -> dict[str, StatType]:
-
     if group == 'control':
       return self.summary_control
     elif group == 'treatment':
