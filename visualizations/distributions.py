d5b38ca6c067553fba60f3274536d1ba88f5e8e4
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-09-10 14:08:26 -0300

    refactor: import GROUP_COLORS from constants.py

--- visualizations/distributions.py
+++ visualizations/distributions.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-
+from constants import GROUP_COLORS
 
 def plot_variable_distributions(
         processed_df: pd.DataFrame,
--- visualizations/proportions.py
+++ visualizations/proportions.py
@@ -7,3 +7,4 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 from constants import GROUP_COLORS
+
